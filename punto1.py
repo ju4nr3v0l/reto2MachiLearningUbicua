fce53687f1c20700c3abb7ca5daf7789d077727b
ju4nr3v0l <jadamavecool@gmail.com>
2023-08-17 20:09:03 -0500

    Resolve challenge2 variant 4

--- punto1.py
+++ punto1.py
@@ -1,3 +1,10 @@
+#1.	Escribe un programa en Python que pregunte el nombre del usuario, su edad y su correo electrónico y luego imprima el siguiente mensaje en pantalla:
+
+#Input: Digite su nombre: Pedro
+#	Digite su edad: 30
+#	Digite su correo electrónico: p3dr1t0_12345@gmail.com
+#Output: Bienvenido al sistema Pedro, su correo electrónico es p3dr1t0_12345@gmail.com y tiene 30 años de edad
+
 def punto1():
     nombre = input("Digite su nombre: ")
     edad = input("Digite su edad: ")
